4d1d8b33a10e3fcd404e4b098e1a324ad9881e80
emberati <alex.shak55@gmail.com>
2020-09-18 13:27:19 +0300

    Добавил суперэллипс

--- src/com/github/emberati/task/kg2020_g21_task1/GradientDrawer.java
+++ src/com/github/emberati/task/kg2020_g21_task1/GradientDrawer.java
@@ -2,7 +2,6 @@ package com.github.emberati.task.kg2020_g21_task1;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.concurrent.CyclicBarrier;
 
 /*
     Муфты, переходники
@@ -21,8 +20,10 @@ public class GradientDrawer {
     /**
      * Рисует многоуровневый градиент цветав colors[] c акцентом цветов в points[]
      * @param polygon       Какую фигуру рисовать градиентом
+     * @param point0        Точка, от которой начинается рисование градиента
+     * @param point1        Точка, в которой заканчивается рисование градиента
+     * @param fractions     Точки градиента (промежуточный градиент от какой точки к какой)
      * @param colors        Массив цветов градиента
-     * @param fractions        Точки градиента (промежуточный градиент от какой точки к какой)
      * @throws NullPointerException     Если один из агрументов равен null
      */
     public Image gradient(Polygon polygon, Point point0, Point point1, float[] fractions, Color[] colors) {
@@ -34,12 +35,26 @@ public class GradientDrawer {
         Rectangle bounds = polygon.getBounds();
         final BufferedImage image = new BufferedImage(bounds.width, bounds.height, BufferedImage.TYPE_INT_RGB);
         final Graphics2D g = image.createGraphics();
-
         MultipleGradientPaint mp = new LinearGradientPaint(point0, point1, fractions, colors, MultipleGradientPaint.CycleMethod.NO_CYCLE);
 
         g.setPaint(mp);
         g.fillPolygon(polygon);
         g.dispose();
+        return image;
+    }
+
+    @Deprecated
+    public Image gradient(Shape shape, Point center, float radius, float[] fractions, Color[] colors) {
+        if (shape == null || center == null || fractions == null) {
+            throw new NullPointerException("Gradient must have a shape, colors and color accent points!");
+        }
+        if (radius < 0) throw new IllegalArgumentException("Radius must be bigger than 0");
+
+        Rectangle bounds = shape.getBounds();
+        final BufferedImage image = new BufferedImage(bounds.width, bounds.height, BufferedImage.TYPE_INT_RGB);
+        final Graphics2D g = image.createGraphics();
+        MultipleGradientPaint mp = new RadialGradientPaint(center, radius, fractions, colors);
+
         return image;
     }
 }
--- src/com/github/emberati/task/kg2020_g21_task1/gui/MainPanel.java
+++ src/com/github/emberati/task/kg2020_g21_task1/gui/MainPanel.java
@@ -4,7 +4,10 @@ import com.github.emberati.task.kg2020_g21_task1.GradientDrawer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
+
+import static java.lang.Math.pow;
 
 public class MainPanel extends JPanel {
 
@@ -16,20 +19,21 @@ public class MainPanel extends JPanel {
 
 
         drawSky(g2d);
-        drawDistrictRect(g2d, 30, 4, 600, 300, 150, getHeight() - 150, new Color(48, 20, 20));
+        //drawDistrictRect(g2d, 40, 4, 400, 200, 500, getHeight() - 150, new Color(52, 56, 56));
 
         g.drawImage(buffer, 0, 0, null);
         g.dispose();
     }
 
     public void drawDistrictRect(Graphics2D g2d, int distanceOy, int houseCount, int x, int y, int width, int height, Color color) {
-        int dy = y;
+
         final double saturation = 1.2;
-        final double brightness = 1.3;
+        final double brightness = 1.25;
 
         for (int i = 0; i < houseCount; i++) {
-            drawPanelHouse(g2d, x, dy, width, height, color);
-            dy += distanceOy;
+            drawPanelHouse(g2d, x, y, width, height, color);
+            y += distanceOy;
+            x -= 10;
 
             color = new Color((int) (color.getRed() * brightness), (int) (color.getGreen() * saturation), (int) (color.getBlue() * saturation));
         }
@@ -41,6 +45,7 @@ public class MainPanel extends JPanel {
     }
 
     private void drawSky(Graphics2D g2d) {
+
         GradientDrawer dw = new GradientDrawer();
         g2d.drawImage(dw.gradient(
                 new Polygon(
@@ -53,17 +58,81 @@ public class MainPanel extends JPanel {
                 new Color[]{Color.BLACK, new Color(7, 12, 96), new Color(66, 155, 122)}
         ), 0, 0, null);
 
-        drawStars(g2d, 100);
+        /*
+        GradientDrawer dw = new GradientDrawer();
+        g2d.drawImage(dw.gradient(
+                new Polygon(
+                        new int[]{0, getWidth(), getWidth(), 0},
+                        new int[]{0, 0, getHeight(), getHeight()},
+                        4
+                ),
+                new Point(getWidth() / 2, -100), new Point(getWidth() / 2, (int) (1.2 * getHeight())),
+                new float[]{.1f, .3f, .4f, .5f, .7f, 1f},
+                new Color[]{Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE}
+        ), 0, 0, null);
+         */
+
+        drawStars(g2d);
     }
 
-    private void drawStars(Graphics2D g2d, int count) {
+    private void drawStars(Graphics2D g2d) {
         //g2d.setColor(Color.YELLOW);
-        final int OFFSET = 20;
+        final int OFFSET = 50;
         Point star = new Point();
-        for (int i = 0; i < count; i++) {
-            star.x = (int) (OFFSET + Math.random() * getWidth() - OFFSET);
-            star.y = (int) (OFFSET + Math.random() * getHeight() - OFFSET);
+        for (int i = 0; i < 100; i++) {
+            star.x = OFFSET + (int) (Math.random() * (getWidth() - 2 * OFFSET));
+            star.y = OFFSET + (int) (Math.random() * (getHeight() - 2 * OFFSET));
+
             g2d.fillOval(star.x, star.y, 3, 2);
         }
+
+        drawEllipse(g2d);
+    }
+
+    void drawEllipse(Graphics2D g)
+    {
+        /*
+         * Скругление
+         * < 1 - вогнутый суперэллипс
+         * > 1 - выпуклый суперэллипс
+         * = 2 - круг
+         */
+        double exp = .3;
+
+        // a ~ радиус опис. ок-ти
+        final int a = 10; // a = b
+        double[] points = new double[a + 1];
+        Point[] coordinates;
+
+        Path2D p = new Path2D.Double();
+        p.moveTo(a, 0);
+
+        // calculate first quadrant
+        for (int x = a; x >= 0; x--) {
+            points[x] = pow(pow(a, exp) - pow(x, exp), 1 / exp); // solve for y
+            p.lineTo(x, -points[x]);
+
+        }
+
+        // mirror to others
+
+        for (int x = 0; x <= a; x++)
+            p.lineTo(x, points[x]);
+
+        for (int x = a; x >= 0; x--)
+            p.lineTo(-x, points[x]);
+
+        for (int x = 0; x <= a; x++)
+            p.lineTo(-x, -points[x]);
+
+        g.translate(getWidth() / 2, getHeight() / 2);
+        g.setStroke(new BasicStroke(2));
+
+        g.setColor(new Color(0x25B0C4DE, true));
+        g.fill(p);
+
+        g.draw(p);
+        g.setStroke(new BasicStroke(1));
+        g.translate(- getWidth() / 2, - getHeight() / 2);
     }
 }
